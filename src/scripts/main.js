478261be79400aa57f0b699accfa941c0b2b4876
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-07-09 19:26:12 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,27 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notif = document.createElement('div');
+  const head = document.createElement('h2');
+  const content = document.createElement('p');
+
+  notif.style.position = 'fixed';
+  notif.setAttribute('style', `top: ${posTop}px; right: ${posRight}px;`);
+
+  notif.classList.add('notification');
+  notif.classList.add(type);
+
+  head.textContent = title;
+  head.classList.add('title');
+  content.textContent = description;
+
+  notif.append(head);
+  notif.append(content);
+  document.body.append(notif);
+
+  setTimeout(() => {
+    notif.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
